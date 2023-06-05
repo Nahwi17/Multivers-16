d22f261834e6ab2e55955fefc910e1d0a19f631a
Nahwi17 <93031476+Nahwi17@users.noreply.github.com>
2023-06-05 12:08:25 +0700

    add 2karakter, oculusion culling, design terain

---
+++ Assets/Scripts/GroundItem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GroundItem : MonoBehaviour
+{
+    public  TextMeshProUGUI triggerText;
+    private void OnTriggerStay(Collider other) {
+        
+    }
+}
