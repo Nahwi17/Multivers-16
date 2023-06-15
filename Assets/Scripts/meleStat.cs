9280197d2472b7d928d4e18253332f42b8ef07e2
YayaIniee <yayainiee@gmail.com>
2023-06-15 23:02:20 +0800

    Update meleStat.cs

--- Assets/Scripts/meleStat.cs
+++ Assets/Scripts/meleStat.cs
@@ -1,28 +1,28 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+// // using System.Collections;
+// // using System.Collections.Generic;
+// // using UnityEngine;
 
-// public class meleStat : MonoBehaviour
-// {
-//    public float damage;
-//    [Range(0,1)]public float maxDistance;
+// // public class meleStat : MonoBehaviour
+// // {
+// //    public float damage;
+// //    [Range(0,1)]public float maxDistance;
 
-//    string tt;
+// //    string tt;
 
-    void Update() {
-        RaycastHit hit;
-        if(Physics.Raycast(transform.position , transform.up, out hit, maxDistance ))  
-        {
-            print("hit: " + hit.transform.name);
-            // hit.transform.root
-        }
-    }
+//     void Update() {
+//         RaycastHit hit;
+//         if(Physics.Raycast(transform.position , transform.up, out hit, maxDistance ))  
+//         {
+//             print("hit: " + hit.transform.name);
+//             // hit.transform.root
+//         }
+//     }
 
-    void private void OnDrawGizmos() {
-        Gizmos.DrawRay(transform.position , transform.up * masDistance);
-    }
+//     void private void OnDrawGizmos() {
+//         Gizmos.DrawRay(transform.position , transform.up * masDistance);
+//     }
 
-//     void OnGUI() {
-//         GUI.Label(new Rect(180, 50, 200, 20), tt);
-//    }
-// }
+// //     void OnGUI() {
+// //         GUI.Label(new Rect(180, 50, 200, 20), tt);
+// //    }
+// // }
